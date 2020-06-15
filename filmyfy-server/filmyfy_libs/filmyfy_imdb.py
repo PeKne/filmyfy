19562a3b9b573fdd84c1ed0a491ad1befdbac62f
Petr Kostka <kostkpe3@fel.cvut.cz>
2020-06-15 15:42:35 +0200

    Finding similar movies using tmdb api

--- filmyfy-server/filmyfy_libs/filmyfy_imdb.py
+++ filmyfy-server/filmyfy_libs/filmyfy_imdb.py
@@ -1,6 +1,6 @@
 from imdb import IMDb
 from imdb._exceptions import IMDbDataAccessError
-
+import requests
 
 class FilmyfyIMDB:
     """
@@ -49,13 +49,47 @@ class FilmyfyIMDB:
         """
                 Gets list of movie ids which are similar to the one (or more) from argument. Maximal list length is 100.
                 :param movie_id: integer or list of integers identifying movies
-                :return: list of movie IDs
+                :return: list of movie IMDB IDs
         """
-        movie_metadata = self.get_movie_metadata(movie_id)
-        keyword_movies_list = []
-        top_250 = self.imdb.get_top250_movies()
+        params = {'language':'en-US',
+                  'api_key':"4de51198adff202147f63d73e4963ee2",
+                  'page':'1'}
+        id = self.convert_to_tmdb_id(movie_id)
+        url = "https://api.themoviedb.org/3/movie/"+str(id)+"/similar"
+        r = requests.get(url = url,params=params)
+        data = r.json()
+        ids = []
+        for d in data['results']:
+            ids.append(self.convert_to_imdb_id(d['id']))
 
-        return keyword_movies_list
+        return ids
+
+    def convert_to_tmdb_id(self, imdb_id):
+        """
+                Converts imbd id to tmbd id.
+                :param imdb_id: integer identifying movie from IMDB
+                :return: id of movie from TMDB
+        """
+        params = {'language':'en-US',
+                  'api_key':"4de51198adff202147f63d73e4963ee2",
+                  'external_source':'imdb_id'}
+
+        url = "https://api.themoviedb.org/3/find/tt"+imdb_id
+
+        r = requests.get(url = url,params=params)
+        return r.json()['movie_results'][0]['id']
+
+    def convert_to_imdb_id(self, tmdb_id):
+        """
+                Converts tmdb id to imdb id.
+                :param imdb_id: integer identifying movie from TMDB
+                :return: id of movie from IMDB
+        """
+        params = {'api_key':"4de51198adff202147f63d73e4963ee2"}
+        url = "https://api.themoviedb.org/3/movie/"+str(tmdb_id)+"/external_ids"
+        r = requests.get(url = url,params=params)
+        return r.json()['imdb_id'][2:]
+
 
 
 
