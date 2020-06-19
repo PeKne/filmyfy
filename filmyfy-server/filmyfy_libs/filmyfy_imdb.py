38e15915718237779820543c0b13a76318a5e7c9
Petr Kostka <kostkpe3@fel.cvut.cz>
2020-06-19 14:39:18 +0200

    Added functioanlity of recommending movies from list of favourite

--- filmyfy-server/filmyfy_libs/filmyfy_imdb.py
+++ filmyfy-server/filmyfy_libs/filmyfy_imdb.py
@@ -3,7 +3,7 @@ import requests
 
 class FilmyfyIMDB:
     """
-    Our base class for communication with IMDb API.
+    Our base class for communication with TMDB API.
     Main use cases: movie retrieving, filtering, getting movie metadata etc.
 
     In __init__ is initialized property `imdb` which is later used for communication with official IMDb API.
@@ -68,18 +68,20 @@ class FilmyfyIMDB:
         url = "https://api.themoviedb.org/3/search/movie"
         r = requests.get(url = url,params=params)
         data = r.json()
-        result = {}
+        result = []
 
         for d in data['results']:
             genres = []
             for g in d['genre_ids']:
                 genres.append(self.genres_list[g])
-            movie ={'title': d['title'],
+            movie ={'id':d['id'],
+                    'title': d['title'],
                     'plot':d['overview'],
                     'rating':d['vote_average'],
-                    'cover url':"http://image.tmdb.org/t/p/w185/"+d['poster_path'],
+                    'cover url':"http://image.tmdb.org/t/p/w185/"+ ("/inVq3FRqcYIRl2la8iZikYYxFNR.jpg"
+                    if d['poster_path'] is None else d['poster_path']),
                     'genres':genres}
-            result[d['id']] = movie
+            result.append(movie)
         return result
 
     def find_similar_movies(self, movie_id):
@@ -94,18 +96,20 @@ class FilmyfyIMDB:
         url = "https://api.themoviedb.org/3/movie/"+str(movie_id)+"/similar"
         r = requests.get(url = url,params=params)
         data = r.json()
-        result = {}
+        result = []
 
         for d in data['results']:
             genres = []
             for g in d['genre_ids']:
                 genres.append(self.genres_list[g])
-            movie ={'title': d['title'],
+            movie ={'id':d['id'],
+                    'title': d['title'],
                     'plot':d['overview'],
                     'rating':d['vote_average'],
-                    'cover url':"http://image.tmdb.org/t/p/w185/"+d['poster_path'],
+                    'cover url':"http://image.tmdb.org/t/p/w185/"+ ("/inVq3FRqcYIRl2la8iZikYYxFNR.jpg"
+                    if d['poster_path'] is None else d['poster_path']),
                     'genres':genres}
-            result[d['id']] = movie
+            result.append(movie)
 
         return result
 
@@ -134,3 +138,18 @@ class FilmyfyIMDB:
         url = "https://api.themoviedb.org/3/movie/"+str(tmdb_id)+"/external_ids"
         r = requests.get(url = url,params=params)
         return r.json()['imdb_id'][2:]
+
+    def find_similar_movie_by_favourite(self, favourite_list_ids):
+        recommended_movies_list = {}
+        for f_id in favourite_list_ids:
+            for d in self.find_similar_movies(f_id):
+                if d['id'] in recommended_movies_list:
+                    recommended_movies_list[d['id']] += 1
+                else:
+                    recommended_movies_list[d['id']] = 1
+
+        sorted_list = sorted(recommended_movies_list.items(), key=lambda kv: -kv[1])
+        result = []
+        for key in sorted_list[:20]:
+            result.append(self.get_movie_metadata(key[0]))
+        return result
